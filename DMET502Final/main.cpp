02cbb83eecd818385ba13d0b2d891f47c1ebcf27
Omar <omarmahrous816@gmail.com>
2025-12-02 20:32:43 +0200

    Initial Commit

---
+++ DMET502Final/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DMET502Final
+//
+//  Created by omar on 02/12/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
